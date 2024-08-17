c2f0d5d463448a4c82660975525c42372c4b3f53
jesus doza <jesus.doza@gmail.com>
2024-08-17 15:25:36 -0500

    add jwt to test request

--- src/test/java/org/company/TestConfig.java
+++ src/test/java/org/company/TestConfig.java
@@ -1,4 +1,18 @@
 package org.company;
 
-public class TestConfig {
-}
+import jdk.dynalink.beans.StaticClass;
+import org.aspectj.weaver.patterns.IToken;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+
+//public class TestConfig {
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8180/realms/workorders/protocol/openid-connect/certs").build();
+//    }
+//}
+//
+
+
--- src/test/java/org/company/UserControllerTest.java
+++ src/test/java/org/company/UserControllerTest.java
@@ -1,11 +1,9 @@
 package org.company;
 
-import org.company.user.UserController;
 import org.company.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,6 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest(classes = WorkOrdersApp.class)
 //@WebMvcTest(UserController.class)
@@ -24,21 +23,37 @@ public class UserControllerTest {
     @Autowired
     UserRepository repository;
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mvc;
 
     @Test
     void givenUnauthenticatedUser_userInfoEndpointReturnsUnauthorized() throws Exception {
-        mockMvc.perform(get("/api/users/me"))
+        mvc.perform(get("/api/user/me"))
                 .andExpect(status().isUnauthorized());
     }
 
     @Test
     void givenAuthenticatedUser_userInfoEndpointReturnsOk() throws Exception {
-        mockMvc.perform(get("/api/users/me")
+        mvc.perform(get("/api/user/me")
                         .with(jwt()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("subject").value("user"))
+                .andExpect(jsonPath("Subject").value("user"))
                 .andExpect(jsonPath("claims").isMap())
                 .andDo(print());
     }
+//    @Test
+//    //test with test token
+//    void givenAuthenticatedUser_userInfoEndpointReturnsOk() throws Exception {
+//        String token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJLdzNkd3g5bElJWTdlLU90cXJBWU5EUjJIV081WVRZX0NPY3hCZFFldmhFIn0.eyJleHAiOjE3MjM5MjQ3ODcsImlhdCI6MTcyMzkyNDQ4NywiYXV0aF90aW1lIjoxNzIzOTI0NDg0LCJqdGkiOiIwNjdmMTAxYi1kNDUzLTRjNTgtYWVmMy0zMzhjYmRiNDFkMTgiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgxODAvcmVhbG1zL3dvcmtvcmRlcnMiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNzlmZjk3MjMtYTIyZC00NTY0LThjNDktN2YxNzk1MDhlMmJlIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoid29ya29yZGVycy1jbGllbnQiLCJzZXNzaW9uX3N0YXRlIjoiODVlMDU3NWItMTM0NS00ZDRiLTllNmYtNDRhYTI4MTYzOTg3IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL29hdXRoLnBzdG1uLmlvIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJkZWZhdWx0LXJvbGVzLXdvcmtvcmRlcnMiLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwic2lkIjoiODVlMDU3NWItMTM0NS00ZDRiLTllNmYtNDRhYTI4MTYzOTg3IiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoid2ltIGV4YW1wbGUiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJ3aW1AZXhhbXBsZS5jb20iLCJnaXZlbl9uYW1lIjoid2ltIiwiZmFtaWx5X25hbWUiOiJleGFtcGxlIiwiZW1haWwiOiJ3aW1AZXhhbXBsZS5jb20ifQ.mCyv_Nths-j_BGRq6v17DjR-rTZn_K-oYI5MgdhlA7NLpyKaliqniVPDfWYrU1DJPY2Lo0oLpwoyXlMtGEg8PwKPDRbItNVButQcRAbCZNcsJ_TFFfRH9DjVRqEg2eHpaXxbn7lTFsPNs9LaW7G2Mp_8ujawUkmNUfkj9fv6tHFU2s8KdlpxjvNvNpq-VI6xY0YKAhgXJSxaqV1mEVLZy6bRG1VCa7rOe9h9WQkAuPgKmKGJbSPLlVP2FBZhGfb1kaVg6q04dxCOVTPl1sDMBvh--r7sCL3sdADdkMTNH25I-JmX6ppfDUl1w7CgL-jqw2dBaR0cQH7h3aCDaR6lBg";
+//
+//
+//        mvc.perform(get("/api/user/me")
+//                        .header("Authorization", "Bearer " + token))
+//                .andExpect(status().isOk());
+////                .andExpect(jsonPath("subject").value("user"))
+////                .andExpect(jsonPath("claims").isMap())
+////                .andDo(print());
+//    }
+
+
 }
+
