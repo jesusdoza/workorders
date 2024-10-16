57d131941f96c84f0b38443108e9278c60f34cd8
jesus doza <jesus.doza@gmail.com>
2024-10-16 10:07:44 -0500

    add navigation sidebar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,12 @@
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 import React from "react";
 
 export default function MainPage() {
-  return <div> MainPage</div>;
+  return (
+    <div>
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
+    </div>
+  );
 }
---
+++ client/src/hooks/useApi.ts
@@ -0,0 +1,9 @@
+async function getUserProfile() {
+  return { userId: "12312" };
+}
+
+async function getServers() {
+  return;
+}
+
+export { getUserProfile, getServers };
