054f44f34e5414538e26465e4d858ac9e0a0c5d2
jesus doza <jesus.doza@gmail.com>
2024-10-23 10:19:51 -0500

    add id to session

--- client/nextAuthConfig.ts
+++ client/nextAuthConfig.ts
@@ -5,10 +5,24 @@ export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   // debug: true,
   callbacks: {
-    async session({ session, token, user }) {
+    async session({ session, token }) {
       // console.log("session callback", session);
+      // console.log("user session callback", user);
+      // console.log("token session callback", token);
 
-      return { userId: token.sub, ...session };
+      return {
+        userId: token.sub,
+        ...session,
+        idToken: token.idToken,
+        authProvider: "google",
+      };
+    },
+    async jwt({ token, account }) {
+      // console.log("user jwt", user);
+      // console.log("token jwt", token);
+      // console.log("account jwt", account);
+      // console.log("profile jwt", profile);
+      return { idToken: account?.id_token, ...token };
     },
   },
   providers: [
@@ -22,7 +36,12 @@ export const authOptions: AuthOptions = {
         // console.log("profile at google provider", profile);
         const { email, sub, given_name, picture } = profile;
 
-        return { id: sub, name: given_name, image: picture, email };
+        return {
+          id: sub,
+          name: given_name,
+          image: picture,
+          email,
+        };
       },
     }),
   ],
