e506b3cd5dbee267cc33d99864ef247b4d0770f6
jesus doza <jesus.doza@gmail.com>
2024-10-24 09:58:41 -0500

    clean up

--- client/nextAuthConfig.ts
+++ client/nextAuthConfig.ts
@@ -3,13 +3,9 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  // debug: true,
+  debug: false,
   callbacks: {
     async session({ session, token }) {
-      // console.log("session callback", session);
-      // console.log("user session callback", user);
-      // console.log("token session callback", token);
-
       return {
         userId: token.sub,
         ...session,
@@ -18,10 +14,6 @@ export const authOptions: AuthOptions = {
       };
     },
     async jwt({ token, account }) {
-      // console.log("user jwt", user);
-      // console.log("token jwt", token);
-      // console.log("account jwt", account);
-      // console.log("profile jwt", profile);
       return { idToken: account?.id_token, ...token };
     },
   },
@@ -33,7 +25,6 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       profile: (profile) => {
-        // console.log("profile at google provider", profile);
         const { email, sub, given_name, picture } = profile;
 
         return {
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,7 @@ import { authOptions } from "../../nextAuthConfig";
 export default async function MainPage() {
   const session = await getServerSession(authOptions);
 
-  console.log("session in page", session);
+  console.log("session in page server component", session);
   return (
     <div>
       <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
