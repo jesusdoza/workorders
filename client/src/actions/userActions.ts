5ea2156048d326046d1218d0ff11d1c8c5161c1c
jesus doza <jesus.doza@gmail.com>
2024-10-30 10:39:43 -0500

    add userActions createUser and getUserProfile scaffold

--- client/src/actions/userActions.ts
+++ client/src/actions/userActions.ts
@@ -1,4 +1,34 @@
 "use server";
 
-export async function createUser() {}
-export async function getUserProfile() {}
+type User = { name: string; email: string; username: string; id: string };
+type UserProfile = User & { organizationId?: string };
+
+export async function createUser({
+  user,
+}: {
+  user: User;
+  authProvider: string;
+  authServerId: string;
+}) {
+  console.log("user createed", user);
+}
+export async function getUserProfile(id: string) {
+  console.log("get user by id", id);
+  return {
+    name: "adf",
+    email: "asfdasdf",
+    id: "safefase",
+    organizationId: "dsafadsf",
+    username: "username",
+  } satisfies UserProfile;
+}
+export async function getUserProfileByAuthId(authId: string) {
+  console.log("get user by auth id", authId);
+  return {
+    name: "adf",
+    email: "asfdasdf",
+    id: "safefase",
+    organizationId: "dsafadsf",
+    username: "username",
+  } satisfies UserProfile;
+}
